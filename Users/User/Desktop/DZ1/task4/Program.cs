fb87a9b8a0dbacd0e27c5fb1d929dfeadd22d17c
AlenaMilkova <azarovahelena@yandex.ru>
2022-11-30 00:16:28 +0300

    Задача номер 8

---
+++ Users/User/Desktop/DZ1/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+while (a > b)
+{
+    Console.Write($"{b}, ");
+    b = b + 2;
+}
+
+    
