efa41b2eed1d16b6bd835ffff836abc989f0c39a
AlenaMilkova <azarovahelena@yandex.ru>
2022-11-29 23:33:49 +0300

    Задача номер 4

---
+++ Users/User/Desktop/DZ1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число1: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число3: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+{
+    if (c > max) max = c;
+    {
+        Console.WriteLine($"Максимальное число, ");
+    Console.Write(max);
+    }
+
+}
+
