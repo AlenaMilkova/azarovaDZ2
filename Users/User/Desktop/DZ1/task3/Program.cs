5bd34d0f3a5b74271850f66caa87cb921bcda97e
AlenaMilkova <azarovahelena@yandex.ru>
2022-11-30 00:01:01 +0300

    Задача номер 6

---
+++ Users/User/Desktop/DZ1/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+{
+    Console.WriteLine($"Число четное, ");
+    Console.Write(a);
+}
+else
+{
+    Console.WriteLine($"Число не является четным, ");
+}
+    
