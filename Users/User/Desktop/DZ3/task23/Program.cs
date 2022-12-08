31e477a47a32199eda8fddf1adf5a4db64e09b08
AlenaMilkova <azarovahelena@yandex.ru>
2022-12-08 21:05:38 +0300

    добавили файл игнор

---
+++ Users/User/Desktop/DZ3/task23/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 23
+//Ввод данных
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+//Функция возведения в куб
+void Cube(int number)
+{
+    number = number * number*number;
+    Console.Write(number + ", "); //вывод результата
+}
+int N = EnterData("Введите число N: ");
+int i = 1;
+while (i <= N)
+{
+    Cube(i);
+    i++;
+} 
+
+
+
+
+
+
+
+
+
